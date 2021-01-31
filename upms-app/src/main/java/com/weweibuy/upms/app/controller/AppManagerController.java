bc553e4b33006b2713952bc92fc4ac0e551cdf84
杜仁浩 <ak514250@126.com>
2021-01-31 14:31:52 +0800

    数据 加密

---
+++ upms-app/src/main/java/com/weweibuy/upms/app/controller/AppManagerController.java
@@ -0,0 +1,19 @@
+package com.weweibuy.upms.app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author durenhao
+ * @date 2021/1/31 14:14
+ **/
+@RestController
+@RequestMapping("/app/manage")
+@RequiredArgsConstructor
+public class AppManagerController {
+
+
+
+
+}

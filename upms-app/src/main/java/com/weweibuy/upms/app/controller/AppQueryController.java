fc873ba4dcb19ab1e361141e861337a764a68038
杜仁浩 <ak514250@126.com>
2021-01-31 13:51:24 +0800

    APP 查询

---
+++ upms-app/src/main/java/com/weweibuy/upms/app/controller/AppQueryController.java
@@ -0,0 +1,33 @@
+package com.weweibuy.upms.app.controller;
+
+import com.weweibuy.framework.common.core.model.dto.CommonDataResponse;
+import com.weweibuy.upms.app.model.dto.resp.AppRespDTO;
+import com.weweibuy.upms.app.service.AppQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+/**
+ * @author durenhao
+ * @date 2021/1/31 13:26
+ **/
+@RestController
+@RequestMapping("/app/query")
+@RequiredArgsConstructor
+public class AppQueryController {
+
+    private final AppQueryService appQueryService;
+
+    @GetMapping("/key")
+    public CommonDataResponse<AppRespDTO> queryByAppKey(String appKey) {
+        return Optional.ofNullable(appKey)
+                .map(appQueryService::queryByAppKey)
+                .map(CommonDataResponse::success)
+                .orElse(CommonDataResponse.success(null));
+    }
+
+
+}
---
+++ upms-app/src/main/java/com/weweibuy/upms/app/service/AppQueryService.java
@@ -0,0 +1,24 @@
+package com.weweibuy.upms.app.service;
+
+import com.weweibuy.framework.common.core.utils.BeanCopyUtils;
+import com.weweibuy.upms.app.model.dto.resp.AppRespDTO;
+import com.weweibuy.upms.app.repository.AppRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author durenhao
+ * @date 2021/1/31 13:28
+ **/
+@Service
+@RequiredArgsConstructor
+public class AppQueryService {
+
+    private final AppRepository appRepository;
+
+    public AppRespDTO queryByAppKey(String appKey) {
+        return appRepository.selectApp(appKey)
+                .map(a -> BeanCopyUtils.copy(a, AppRespDTO.class))
+                .orElse(null);
+    }
+}
